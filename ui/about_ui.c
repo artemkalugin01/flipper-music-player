93f16248312f302104c6da0b9ac9111539de64ce
artemkalugin01 <40526130+artemkalugin01@users.noreply.github.com>
2025-07-08 15:07:15 +0300

    "about" pane done + .version added (#2)
    "about" pane done + .version added

--- ui/about_ui.c
+++ ui/about_ui.c
@@ -4,21 +4,43 @@
 #include <input/input.h>
 
 
-void draw_about_pane(Canvas *canvas, void *context) {
+void draw_about_pane(Canvas* canvas, void* context) {
   furi_assert(context);
 
   canvas_clear(canvas);
+  canvas_set_color(canvas, ColorBlack);
+
+  // To center text, we calculate the starting X position for each string.
+  // X = (Screen Width - String Width) / 2
+  const uint8_t screen_width = 64;
+  uint8_t str_width;
+  uint8_t x;
+
   canvas_set_font(canvas, FontPrimary);
+  const char* text_music = "Music";
+  str_width = canvas_string_width(canvas, text_music);
+  x = (screen_width - str_width) / 2;
+  canvas_draw_str(canvas, x, 12, text_music);
 
-  canvas_draw_box(canvas, 0, 0, 64, 12);
-  canvas_set_color(canvas, ColorWhite);
-  canvas_draw_str(canvas, 2, 12, "About section");
+  canvas_set_font(canvas, FontKeyboard);
+  const char* text_aah = "aah";
+  str_width = canvas_string_width(canvas, text_aah);
+  x = (screen_width - str_width) / 2;
+  canvas_draw_str(canvas, x, 26, text_aah);
 
-  canvas_set_color(canvas, ColorXOR);
-  canvas_draw_str(canvas, 2, 36, "Press back");
+  canvas_set_font(canvas, FontPrimary);
+  const char* text_player = "Player";
+  str_width = canvas_string_width(canvas, text_player);
+  x = (screen_width - str_width) / 2;
+  canvas_draw_str(canvas, x, 38, text_player);
 
-  // test
+  const char* text_for_8bit = "For 8-bit";
+  str_width = canvas_string_width(canvas, text_for_8bit);
+  x = (screen_width - str_width) / 2;
+  canvas_draw_str(canvas, x, 52, text_for_8bit);
 
-  canvas_draw_str(canvas, 2, 60, "XX");
-  canvas_draw_str(canvas, 60, 2, "YY");
+  const char* text_music_lower = "music";
+  str_width = canvas_string_width(canvas, text_music_lower);
+  x = (screen_width - str_width) / 2;
+  canvas_draw_str(canvas, x, 62, text_music_lower);
 }
